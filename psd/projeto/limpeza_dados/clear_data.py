a1d385b9ec4ae2c5dbfba8b344e647b81770a322
MatheusDemiro <mpdemiro@hotmail.com>
2018-10-25 16:48:13 -0300

    Clear Data

--- psd/projeto/limpeza_dados/clear_data.py
+++ psd/projeto/limpeza_dados/clear_data.py
@@ -2,10 +2,17 @@ import datetime
 import time
 
 def formatTimestamp(array):
-    hour = str(int(array[1][0:2])-3)
-    string = array[0]+" "+hour+array[1][2:]
-
-    return string
+    hour = array[1][0:2]
+    if hour == "00":
+        time = "21"
+    elif hour == "01":
+        time = "22"
+    elif hour == "02":
+        time = "23"
+    else:
+        time = str(int(hour)-2)
+    print(array[0]+" "+time+array[1][2:])
+    return array[0]+" "+time+array[1][2:]
 
 cassandra2 = open("Cassandra2.TXT","r+")
 newCassandra2 = open("newCassandra2.txt","w+")
@@ -14,20 +21,29 @@ data = cassandra2.readlines()
 
 cassandra2.close()
 
-aux = []
-
 #Falta condicao para hora "00","01" e "02", pois como subtraimos 3 para seguir o padrao UTC a hora fica com valor negativo.
 #Verificar se as condição para as datas "0/0" estão funcionais
+
 
 for information in data:
     temp = information.split(" - ")
     temp = temp[1].split(" | ") #Cortar a lista apenas com os dados de interesse (hora e data) para formar o timestamp
-    if temp[0] == "0/0":
-        temp[0] = aux[-1] #Adicionando a ultima data adicionada
-        print(temp[0])
-    else:
-        temp[0] = temp[0]+"/2018" #Adicionando o ano de 2018
-        if temp[0] not in aux: #Minimizando o comprimento da lista auxiliar
-            aux.append(temp[0])#Guardando a data para o caso de datas 0/0
-    newCassandra2.writelines(str(int(time.mktime(datetime.datetime.strptime(formatTimestamp(temp), "%d/%m/%Y %H:%M:%S").timetuple()))))
+    if temp[0] == "0/0" or temp[0] == "0/36":
+        continue        
+    temp[0] = temp[0]+"/2018" #Adicionando o ano de 2018
+    newCassandra2.writelines(str(int(time.mktime(datetime.datetime.strptime(formatTimestamp(temp), "%d/%m/%Y %H:%M:%S").timetuple())))+"\n")
 newCassandra2.close()
+'''
+#Reverse engineering
+
+from datetime import datetime
+
+a = open("newCassandra2.txt","r")
+
+b = a.readlines()
+
+a.close()
+
+for i in b:
+    print(datetime.utcfromtimestamp(int(i[0:-1])).strftime('%d/%m/%Y %H:%M:%S'))
+'''
