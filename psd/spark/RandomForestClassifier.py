a111bf233becb7a92a468fba74234f4d6ca95832
Unknown <mpdemiro@hotmail.com>
2018-11-25 22:13:28 -0200

    Correção na modularização

--- psd/spark/RandomForestClassifier.py
+++ psd/spark/RandomForestClassifier.py
@@ -1,56 +1,63 @@
+import findspark
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
-from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
+from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer, VectorAssembler
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
 
-import findspark
+findspark.init("C:\spark-2.4.0-bin-hadoop2.7")
 
-findspark.init("/home/rsi-psd-vm/spark-2.2.0-bin-hadoop2.7")
+class RandomForest():
 
-spark = SparkSession.builder\
-    .master("local")\
-    .appName("Test Spark")\
-    .getOrCreate()
+    def __init__(self):
+        pass
 
-# Load and parse the data file, converting it to a DataFrame.
-data = spark.read.format("csv").load("/home/rsi-psd-vm/Downloads/dados-psd-2018-2.csv")
+    def getContext(self):
+        spark_context = SparkSession.builder \
+            .master("local") \
+            .appName("Spark Demo") \
+            .getOrCreate()
 
-# Index labels, adding metadata to the label column.
-# Fit on whole dataset to include all labels in index.
-labelIndexer = StringIndexer(inputCol="Classe", outputCol="indexedLabel").fit(data)
+        return spark_context
 
-# Automatically identify categorical features, and index them.
-# Set maxCategories so features with > 4 distinct values are treated as continuous.
-featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+    def generateData(self):
 
-# Split the data into training and test sets (30% held out for testing)
-(trainingData, testData) = data.randomSplit([0.7, 0.3])
+        data = self.getContext().read.format("csv").option("header", "true").option("inferSchema", "true").load(
+            "C:\Users\User\Documents\UFRPE\Arquivos_2018.2\dados-psd-2018-2.csv")
 
-# Train a RandomForest model.
-rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", numTrees=10)
+        data = self.convertColumn(data, data.columns[:-1])
 
-# Convert indexed labels back to original labels.
-labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
-                               labels=labelIndexer.labels)
+        return data
 
-# Chain indexers and forest in a Pipeline
-pipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf, labelConverter])
+    def convertColumn(self, dataframe, columns):
+        for name in columns:
+            if name == 'ts':
+                dataframe = dataframe.withColumn(name, dataframe[name].cast(IntegerType()))
+            else:
+                dataframe = dataframe.withColumn(name, dataframe[name].cast(DoubleType()))
+        return dataframe
 
-# Train model.  This also runs the indexers.
-model = pipeline.fit(trainingData)
+    def randomForestClassifier(self, data, trainingData):
 
-# Make predictions.
-predictions = model.transform(testData)
+        vecAssembler = VectorAssembler(inputCols=data.columns[:-1], outputCol="features").setHandleInvalid("keep")
 
-# Select example rows to display.
-predictions.select("predictedLabel", "label", "features").show(5)
+        labelIndexer = StringIndexer(inputCol="classe", outputCol="indexedLabel").fit(data)
 
-# Select (prediction, true label) and compute test error
-evaluator = MulticlassClassificationEvaluator(
-    labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
-accuracy = evaluator.evaluate(predictions)
-print("Test Error = %g" % (1.0 - accuracy))
+        featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4)
 
-rfModel = model.stages[2]
-print(rfModel)  # summary only
+        rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", numTrees=100)
+
+        labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
+                                       labels=labelIndexer.labels)
+
+        pipeline = Pipeline(stages=[vecAssembler, labelIndexer, featureIndexer, rf, labelConverter])
+
+        return pipeline.fit(trainingData)
+
+    def getError(self, predictions):
+        evaluator = MulticlassClassificationEvaluator(
+            labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+        accuracy = evaluator.evaluate(predictions)
+
+        return "Test Accuracy Rate: %g\nTest Error Rate: %g" % (accuracy, 1.0 - accuracy)
--- psd/spark/run.py
+++ psd/spark/run.py
@@ -1,10 +1,5 @@
 from RandomForestClassifier import RandomForest
 
-def getError():
-    accuracy = rf_instance.getAcurrancy(predictions)
-
-    return "Test Accuracy Rate: %g\nTest Error Rate: %g" % (accuracy, 1.0 - accuracy)
-
 rf_instance = RandomForest()
 
 data = rf_instance.generateData()
@@ -17,4 +12,4 @@ predictions = randomForestModel.transform(testData)
 
 predictions.select("predictedLabel", "classe", "features").show(95, False)
 
-print(getError())
+print(rf_instance.getError(predictions))
