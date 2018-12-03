27ef12d1c07f04a8588705c6ef7bc8645dda1ef6
Unknown <mpdemiro@hotmail.com>
2018-12-03 03:17:08 -0200

    Atualização script

--- rsi/Projeto sniffer/connection_serial.py
+++ rsi/Projeto sniffer/connection_serial.py
@@ -8,12 +8,18 @@ def header(data):
     RSSI = first[0]
     TS = int(first[2])
     MAC = first[1]
-    dic = {MAC:(int(RSSI),1)}
+    dic = {MAC:([int(RSSI)],1)}
 
     return dic, MAC, TS
 
 def setTimestamp(data):
     return data.decode()[:-2]+","+str(int(time.time()))
+
+def average(dic):
+    for i in dic:
+        temp = dic[i]
+        dic[i] = (sum(temp[0])/temp[1], temp[1])
+    return dic
 
 comport = serial.Serial('com10', 115200)
 arq = open("data_sniffer.txt", "w+")
@@ -31,17 +37,15 @@ while True:
         if (int(TS) - windowTS) < 300:
             if currentMAC in dic:
                 temp = dic[currentMAC]
-                if temp[1] > 1:
-                    dic[currentMAC] = (temp[0] + (int(RSSI)/2), temp[1] + 1)
-                else:
-                    dic[currentMAC] = ((temp[0] + int(RSSI))/2, temp[1] + 1)
+                temp[0].append(int(RSSI))
+                dic[currentMAC] = (temp[0], temp[1] + 1)
             else:
-                dic[currentMAC] = (int(RSSI), 1)
+                dic[currentMAC] = ([int(RSSI)], 1)
             currentTS = int(TS)
         else:  # 5 minutes (time-window)
-            windows.append(dic)
+            windows.append(average(dic))
             windowTS = int(TS)
-            dic = {MAC: (int(RSSI), 1)}
+            dic = {MAC: ([int(RSSI)], 1)}
             currentTS = int(TS)
     print(dic)
     arq.writelines(data+"\n")
